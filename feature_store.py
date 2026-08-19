23dfd4fdcb2d36c485db3e0930c97eaf1e79c722
Muhib8604 <rashidmuhib37@gmail.com>
2026-08-19 12:51:20 +0500

    update

--- feature_store.py
+++ feature_store.py
@@ -4,12 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def connect_feature_store():
+    cert_folder = os.path.join(os.getcwd(), "hops_certs")
+    os.makedirs(cert_folder, exist_ok=True)
+
     project = hopsworks.login(
         project=os.getenv("HOPSWORKS_PROJECT"),
         api_key_value=os.getenv("HOPSWORKS_API_KEY"),
         engine="python",
-        cert_folder=r"E:\AQI Predictor\hops_certs",
+        cert_folder=cert_folder,
     )
 
     return project.get_feature_store()
